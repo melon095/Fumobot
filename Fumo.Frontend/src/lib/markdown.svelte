f3a85e3716e86167e18f32b0b224b2af22d9ed32
Joachim Flottorp <joachim@flottorp.org>
2024-01-31 23:58:05 +0100

    Fix some padding and format

--- Fumo.Frontend/src/lib/markdown.svelte
+++ Fumo.Frontend/src/lib/markdown.svelte
@@ -1,12 +1,10 @@
 <script lang="ts">
-    import {parse} from 'marked';
+	import { parse } from 'marked';
 
-    export let markdown: string;
+	export let markdown: string;
 
-    const sanitizeHtml = (html: string) => ({__html: html});
-
-    // Only Promise<string> if extension is async.
-    $: markedHtml = (parse(markdown) as string);
+	// Only Promise<string> if extension is async.
+	$: markedHtml = parse(markdown) as string;
 </script>
 
 {@html markedHtml}
--- Fumo.Frontend/src/routes/commands/+page.svelte
+++ Fumo.Frontend/src/routes/commands/+page.svelte
@@ -1,132 +1,155 @@
 <script lang="ts">
-    import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+	import {
+		Table,
+		TableBody,
+		TableBodyCell,
+		TableBodyRow,
+		TableHead,
+		TableHeadCell
+	} from 'flowbite-svelte';
 	import { onMount } from 'svelte';
 	import { pushState } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Markdown from '$lib/markdown.svelte';
-    import type { PageData } from './$types';
+	import type { PageData } from './$types';
 	import type { CommandModal } from '$lib/types';
 
-    export let data: PageData;
+	export let data: PageData;
 
-    const commands = data.props.commands;
+	const commands = data.props.commands;
 
-    const detailCache = new Map<string, CommandModal>();
-    let openRow = -1;
-    let details: CommandModal | undefined;
-    let searchTerm = '';
+	const detailCache = new Map<string, CommandModal>();
+	let openRow = -1;
+	let details: CommandModal | undefined;
+	let searchTerm = '';
 
-    async function toggleRow(i: number) {
-        if (openRow == i) {
-            openRow = -1;
-            details = undefined;
-            return;
-        }
+	async function toggleRow(i: number) {
+		if (openRow == i) {
+			openRow = -1;
+			details = undefined;
+			return;
+		}
 
-        const query = new URLSearchParams(window.location.search);
-        query.set('c', commands[i].name);
-        
-        pushState(`?${query}`, $page.state);
+		const query = new URLSearchParams(window.location.search);
+		query.set('c', commands[i].name);
 
-        details = await fetchDetails(commands[i].name);
-        openRow = i;
-    }
+		pushState(`?${query}`, $page.state);
 
-    async function fetchDetails(name: string) {
-        if (detailCache.has(name)) {
-            return detailCache.get(name);
-        }
-        
-        const res = await fetch(`/api/commands/${name}`);
-        const json = await res.json();
+		details = await fetchDetails(commands[i].name);
+		openRow = i;
+	}
 
-        detailCache.set(name, json);
+	async function fetchDetails(name: string) {
+		if (detailCache.has(name)) {
+			return detailCache.get(name);
+		}
 
-        return json;
-    }
+		const res = await fetch(`/api/commands/${name}`);
+		const json = await res.json();
 
-    onMount(async () => {
-        // check if there is a "c" query param, if so set the row
-        const query = new URLSearchParams(window.location.search);
-        const command = query.get('c');
+		detailCache.set(name, json);
 
-        if (!command) return;
+		return json;
+	}
 
-        const index = commands.findIndex(c => c.name == command);
+	onMount(async () => {
+		// check if there is a "c" query param, if so set the row
+		const query = new URLSearchParams(window.location.search);
+		const command = query.get('c');
 
-        if (index == -1) return;
+		if (!command) return;
 
-        details = await fetchDetails(command);
-        openRow = index;
-    });
+		const index = commands.findIndex((c) => c.name == command);
 
-    $: filteredCommands = (!searchTerm) 
-        ? commands 
-        : commands.filter(c => c.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		if (index == -1) return;
 
+		details = await fetchDetails(command);
+		openRow = index;
+	});
+
+	$: filteredCommands = !searchTerm
+		? commands
+		: commands.filter((c) => c.name.toLowerCase().includes(searchTerm.toLowerCase()));
 </script>
-
-<style>
-    :global(#cmd-sct) {
-        max-width: 62rem;
-        margin: 0 auto;
-        padding: 0 1rem;
-        padding-bottom: 1rem;
-    }
-</style>
 
 <!-- Note: I know TableSearch exists. :) -->
 
 <section id="cmd-sct">
-    
-<div class="relative overflow-x-auto shadow-md">
-    <div class="p-4">
-        <label for="cmd-search" class="sr-only">Search</label>
-    <div class="relative mt-1">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-            <slot name="svgSearch">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                </svg>
-            </slot>
-            </div>
-            <input bind:value={searchTerm} type="text" placeholder="Search by name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 ps-10">
-        </div>
-    </div>
-</div>
+	<div class="relative overflow-x-auto shadow-md">
+		<div class="p-4">
+			<label for="cmd-search" class="sr-only">Search</label>
+			<div class="relative mt-1">
+				<div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+					<slot name="svgSearch">
+						<svg
+							class="w-5 h-5 text-gray-500 dark:text-gray-400"
+							fill="currentColor"
+							viewBox="0 0 20 20"
+							xmlns="http://www.w3.org/2000/svg"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</slot>
+				</div>
+				<input
+					bind:value={searchTerm}
+					type="text"
+					placeholder="Search by name"
+					class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-80 p-2.5 ps-10"
+				/>
+			</div>
+		</div>
+	</div>
 
-<Table class="shadow-md">
-    <TableHead>
-        <TableHeadCell>Name</TableHeadCell>
-        <TableHeadCell>Description</TableHeadCell>
-        <TableHeadCell>Cooldown</TableHeadCell>
-    </TableHead>
-    <TableBody tableBodyClass="divide-y">
-        {#each filteredCommands as command, i}
-        <TableBodyRow on:click={() => toggleRow(i)} class="no-underline select-none cursor-pointer hover:bg-blue-100">
-                <TableBodyCell>{command.name}</TableBodyCell>
-                <TableBodyCell>{command.description}</TableBodyCell>
-                <TableBodyCell>{command.cooldown} Seconds</TableBodyCell>
-            </TableBodyRow>
+	<Table class="shadow-md">
+		<TableHead>
+			<TableHeadCell>Name</TableHeadCell>
+			<TableHeadCell>Description</TableHeadCell>
+			<TableHeadCell>Cooldown</TableHeadCell>
+		</TableHead>
+		<TableBody tableBodyClass="divide-y">
+			{#each filteredCommands as command, i}
+				<TableBodyRow
+					on:click={() => toggleRow(i)}
+					class="no-underline select-none cursor-pointer hover:bg-blue-100"
+				>
+					<TableBodyCell>{command.name}</TableBodyCell>
+					<TableBodyCell>{command.description}</TableBodyCell>
+					<TableBodyCell>{command.cooldown} Seconds</TableBodyCell>
+				</TableBodyRow>
 
-            {#if openRow == i}
-            <TableBodyRow id="drawer-row">
-                    <TableBodyCell colspan="3">
-                        <div class="overflow-auto whitespace-pre-wrap pt-0 pb-0 pl-6 pr-6 overflow-x-auto text-left border border-solid border-gray-300">
-                            <div class="flex flex-col text-black">
-                                <p>Regex: {details?.regex}</p>
-                                <p>Permissions: {details?.permission}</p>
-                                
-                                {#if details?.description}
-                                    <div><Markdown markdown={details?.description}/></div>
-                                    {/if}
-                                </div>
-                            </div>
-                        </TableBodyCell>
-                    </TableBodyRow> 
-            {/if}
-        {/each}
-    </TableBody>
-</Table>
+				{#if openRow == i}
+					<TableBodyRow id="drawer-row">
+						<TableBodyCell colspan="3">
+							<div
+								class="overflow-auto whitespace-pre-wrap pt-6 pb-2 pl-6 pr-6 overflow-x-auto text-left border border-solid border-gray-300"
+							>
+								<div class="flex flex-col text-black">
+									<p>Regex: {details?.regex}</p>
+									<p>Permissions: {details?.permission}</p>
 
+									{#if details?.description}
+										<div><Markdown markdown={details?.description} /></div>
+									{/if}
+								</div>
+							</div>
+						</TableBodyCell>
+					</TableBodyRow>
+				{/if}
+			{/each}
+		</TableBody>
+	</Table>
 </section>
+
+<style>
+	:global(#cmd-sct) {
+		max-width: 62rem;
+		margin: 0 auto;
+		padding: 0 1rem;
+		padding-bottom: 1rem;
+	}
+</style>
