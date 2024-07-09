20302dda53726afceb8074badb7d98fece5df176
Joachim Flottorp <joachim@flottorp.org>
2024-07-09 21:52:14 +0200

    Remove commented frontend code

--- src/Fumo.Frontend/src/routes/+page.server.ts
+++
@@ -1,6 +0,0 @@
-// import { redirect } from '@sveltejs/kit';
-// import type { PageServerLoad } from './$types';
-
-// export const load: PageServerLoad = () => {
-// 	return redirect(302, '/commands/');
-// };
