584078896ccdea470938a986af6f8bcef8ccd22b
Joachim Flottorp <joachim@flottorp.org>
2025-02-22 17:27:43 +0100

    xd

--- src/Fumo.Commands/SevenTV/SevenTVRemoveCommand.cs
+++ src/Fumo.Commands/SevenTV/SevenTVRemoveCommand.cs
@@ -47,7 +47,7 @@ public class SevenTVRemoveCommand : ChatCommand
 
         foreach (var emote in currentEmotes)
         {
-            if (!Input.Remove(emote.Name) || !Input.Remove(emote.ID))
+            if (!Input.Remove(emote.Name) && !Input.Remove(emote.ID))
                 continue;
 
             emotesToRemove.Add(emote);
